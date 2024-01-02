b8ebaa7e832cf16da614294ab383030c19df04fb
Alex Isaacs <alexisaacs18@gmail.com>
2024-01-02 16:44:29 -0500

    updated files

--- lib/models/room.py
+++ lib/models/room.py
@@ -1,3 +1,5 @@
+from connect import CURSOR, CONN
+
 
 class Room:
     def __init__(self, name, locked, description):
@@ -5,4 +7,53 @@ class Room:
         self.locked = locked
         self.description = description
 
-    # Add name and locked attributes to name chart in SQL
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS rooms
+            (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                locked BOOLEAN
+            );
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS rooms;
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+
+Room.drop_table()
+Room.create_table()
+
+room_one = """
+    INSERT INTO 
+    rooms(name, locked) 
+    VALUES('Bedroom', true)
+"""
+CURSOR.execute(room_one)
+CONN.commit()
+
+room_two = """
+    INSERT INTO
+    rooms(name, locked)
+    VALUES('Hallway', false)
+"""
+CURSOR.execute(room_two)
+CONN.commit()
+
+room_three = """
+    INSERT INTO
+    rooms(name, locked)
+    VALUES('Library', false)
+"""
+CURSOR.execute(room_three)
+CONN.commit()
+
+# Add name and locked attributes to name chart in SQL
